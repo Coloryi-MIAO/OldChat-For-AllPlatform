8906361df52131477e599e7500e1ab2b196d7eba
Coloryi-MIAO <Shiroko_white@outlook.com>
2026-08-20 08:11:52 +0000

    fix: use valid raw regex literals

--- lib/utils/user_error.dart
+++ lib/utils/user_error.dart
@@ -1,7 +1,7 @@
 String safeErrorMessage(Object error) {
   var message = error.toString();
-  message = message.replaceAll(RegExp(r"file\s+['\"]?[^'\"]+['\"]?"), 'file');
-  message = message.replaceAll(RegExp(r"/(?:home|Users|storage|private)/[^\s'\"]+"), 'local file');
-  message = message.replaceAll(RegExp(r"[A-Za-z]:\\[^\s'\"]+"), 'local file');
+  message = message.replaceAll(RegExp(r'''file\s+['"]?[^'"]+['"]?'''), 'file');
+  message = message.replaceAll(RegExp(r'''/(?:home|Users|storage|private)/[^\s'"]+'''), 'local file');
+  message = message.replaceAll(RegExp(r'''[A-Za-z]:\\[^\s'"]+'''), 'local file');
   return message.length > 240 ? '${message.substring(0, 240)}…' : message;
 }
