8e088a176fc22db847a967901e497edf6b9b172b
Coloryi-MIAO <Shiroko_white@outlook.com>
2026-08-20 08:09:16 +0000

    fix: correct platform error sanitizer

--- lib/utils/user_error.dart
+++ lib/utils/user_error.dart
@@ -1,6 +1,6 @@
 String safeErrorMessage(Object error) {
   var message = error.toString();
-  message = message.replaceAll(RegExp(r"file ['\"]?[^'\"]+['\"]?"), 'file');
+  message = message.replaceAll(RegExp(r"file\s+['\"]?[^'\"]+['\"]?"), 'file');
   message = message.replaceAll(RegExp(r"/(?:home|Users|storage|private)/[^\s'\"]+"), 'local file');
   message = message.replaceAll(RegExp(r"[A-Za-z]:\\[^\s'\"]+"), 'local file');
   return message.length > 240 ? '${message.substring(0, 240)}…' : message;
