6df1b17950ef005a9192130c08fa89b8d9c9635c
Coloryi-MIAO <Shiroko_white@outlook.com>
2026-08-24 05:14:25 +0000

    fix Windows 7 registration layout and gestures

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -549,7 +549,8 @@ class _LoginPageState extends State<LoginPage>
                   ],
                 ),
               ),
-            );
+            ),
+          );
             return compact
                 ? SingleChildScrollView(
                     keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
