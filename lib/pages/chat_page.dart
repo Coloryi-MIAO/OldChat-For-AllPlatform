208ab6db70d2b5c2ce0a60b1871c8736e3fe8c3a
Coloryi-MIAO <Shiroko_white@outlook.com>
2026-08-24 04:55:05 +0000

    fix pointer gesture import across platforms

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:dio/dio.dart';
